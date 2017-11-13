eb459fc335d98d46e7e4fba633fd8833180e5436
Mint Cinimmon <bexsive@gmail.com>
2017-11-13 11:15:40 -0800

    Added Chef model validations with tests

---
+++ db/migrate/20171108191912_change_column_in_recipies.rb
@@ -0,0 +1,7 @@
+class ChangeColumnInRecipies < ActiveRecord::Migration[5.1]
+  def change
+
+    rename_column :recipes, :email, :description
+    change_column :recipes, :description, :text
+  end
+end
