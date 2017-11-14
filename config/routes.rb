8019122a3594a79ce57011965a1d87d1e57f6765
Mint Cinimmon <bexsive@gmail.com>
2017-11-14 12:12:18 -0800

    Added Gravatar and styling to recipes, partial for title

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'pages/home', to: 'pages#home'
 
   get '/recipes', to: 'recipes#index'
+  get '/recipes/:id', to: 'recipes#show', as: 'recipe'
+
+
   #resources :recipes
 
 end
