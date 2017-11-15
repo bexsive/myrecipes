28274cf5d128c8f8ac1656ca54124e4c1a5e2958
Mint Cinimmon <bexsive@gmail.com>
2017-11-15 11:28:30 -0800

    Refactoring into partials, helpers

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,12 @@
 class RecipesController < ApplicationController
-
+  before_action :set_recipe, only: [:show, :edit, :update]
   def index
     #@chef =
     @recipes = Recipe.all
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+
   end
 
   def new
@@ -26,11 +26,11 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
+
   end
 
   def update
-    @recipe = Recipe.find(params[:id])
+
     if @recipe.update(recipe_params)
       flash[:success] = "Recipe was updated successfully!"
       redirect_to recipe_path(@recipe)
@@ -46,13 +46,18 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id]).destroy
     flash[:succes] = "Recipe deleted successfully"
     redirect_to recipes_path
-    
+
   end
 
 
 
 
   private
+
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
     def recipe_params
       params.require(:recipe).permit(:name, :description)
     end
